0b82e0a968c920f9fc2aa50f119a434a8e287185
Brad Comp Sci <bjbaudin@cox.net>
2019-03-18 13:58:51 -0500

    umm

--- 20 Inheritance/OverRide.java
+++ 20 Inheritance/OverRide.java
@@ -5,44 +5,58 @@
 
 import static java.lang.System.*;
 
+public class OverRide
+{
+  public static void main ( String[] args )
+  {
+     Monster ghost = new Ghost("Casper");
+     out.println(ghost);
+     ghost.overRide();
+
+     Monster witch = new Witch("Harriet");
+     out.println(witch);
+     witch.overRide();
+  }
+}
+
 class Monster
 {
-	private String myName;   	// inherited, but not accessible in sub class
+ private String myName;    // inherited, but not accessible in sub class
 
-	public Monster() { }
+ public Monster() { }
 
-	public Monster( String name )
-	{
-		myName = name;
-	}
+ public Monster( String name )
+ {
+  myName = name;
+ }
 
-	public void overRide( )
-	{
-		System.out.println("overRide in Monster\n");
-	}
+ public void overRide( )
+ {
+  System.out.println("overRide in Monster\n");
+ }
 
-	public String toString()
-	{
-		return "Monster name :: " + myName + "\n";
-	}
+ public String toString()
+ {
+  return "Monster name :: " + myName + "\n";
+ }
 }
 
 class Witch extends Monster
 {
-	public Witch( String name )
-	{
-		super(name);
-	}
+ public Witch( String name )
+ {
+  super(name);
+ }
 
-	public void overRide( )
-	{
-		System.out.println("overRide in Witch\n");
-	}
+ public void overRide( )
+ {
+  System.out.println("overRide in Witch\n");
+ }
 
-	public String toString()
-	{
-		return "Witches are the best!!!!!\n";
-	}
+ public String toString()
+ {
+  return "Witches are the best!!!!!\n";
+ }
 }
 
 class Ghost extends Monster
@@ -57,7 +71,7 @@ class Ghost extends Monster
    public int overRide( )
    {
       System.out.println("overRide in Ghost");
-      return 0;		///attempting to use incompatible return type
+      return 0;  ///attempting to use incompatible return type
    }*/
 
 
@@ -68,16 +82,3 @@ class Ghost extends Monster
 }
 
 
-public class OverRide
-{
-  public static void main ( String[] args )
-  {
-     Monster ghost = new Ghost("Casper");
-     out.println(ghost);
-     ghost.overRide();
-
-     Monster witch = new Witch("Harriet");
-     out.println(witch);
-     witch.overRide();
-  }
-}
